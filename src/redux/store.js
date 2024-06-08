6129e1c8c5fbc7a10917de77e6e3a7d715338611
Vlad-Leskov <tyabaka@gmail.com>
2024-06-08 18:18:25 +0300

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,4 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
@@ -17,24 +7,8 @@ const rootReducer = combineReducers({
   filters: filtersReducer,
 });
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["contacts"], // Зберігати тільки контакти
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer,
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export { store };
